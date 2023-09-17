f6887c564e3f7f4afe0e28e7618621102677531f
Sheng.T <>
2023-09-17 18:11:05 +0300

    improvement: format, readability

--- Module_7/7_3.py
+++ Module_7/7_3.py
@@ -1,14 +1,15 @@
-def check_dup(airport_dict, icao):
+def confirm_add(airport_dict, icao):
     if icao not in airport_dict.keys():
-        return 1
+        return True
     else:
         overwrite = input("ICAO already exist, do you want to overwrite? (y/n): ")
         if overwrite == "y":
-            return 1
+            return True
         elif overwrite == "n":
             print("Skipped.")
         else:
             print("Invalid option, skipped.")
+    return False
 
 
 def add_airport(airport_dict, icao, airport_name):
@@ -27,20 +28,19 @@ if __name__ == "__main__":
     airport_dict_1 = {}
 
     while True:
-        print("\nOptions: 1.New airport. 2.Fetch information of airport. 3.Quit: ", end="")
-        option = int(input())
+        option = input("\nOptions: 1.Add new airport. 2.Fetch information of airport. 3.Quit: ")
 
-        if option == 1:
+        if option == "1":
             icao = input("Enter the ICAO: ")
-            if check_dup(airport_dict_1, icao) == 1:
+            if confirm_add(airport_dict_1, icao) == True:
                 airport_name = input("Enter the name of airport: ")
                 add_airport(airport_dict_1, icao, airport_name)
 
-        elif option == 2:
+        elif option == "2":
             icao = input("Enter the ICAO: ")
             print(fetch_airport(airport_dict_1, icao))
 
-        elif option == 3:
+        elif option == "3":
             print("Goodbye")
             break
 
